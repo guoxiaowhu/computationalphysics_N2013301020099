5a2b97b9cebf1438d0ca76e2b425eff115accbe4
guoxiaowhu <guoxiao1995@qq.com>
2016-04-23 22:03:05 +0800

    Added files via upload

--- problem1.6.py
+++ problem1.6.py
@@ -8,34 +8,55 @@ Author:GUO Xiao
 import numpy as np
 from pylab import *
 from math import *
-
-N=[]
+time=2
+dt=0.001
 t=[]
+for i in range(int(time/dt)+1):
+    t.append(i*dt)
+#numerical solution
+def solve_N(a,b):
+    N=[]
+    t=[]
+    N_0=100#float(input('N0='))
+    N.append(N_0)
+    t.append(0)
+    time=2#float(input('total time='))
+    dt=0.001#float(input('time step='))
+    for i in range(int(time/dt)):
+        N.append(N[i]+dt*(a*N[i]-b*(N[i])**2))#Attn:It is'**',not'^'
+        t.append(t[i]+dt)
+    return N
+N_0=solve_N(1,0)
+N_1=solve_N(1,0.01)
+N_2=solve_N(1,0.02)
 
-#input
-a=float(input('a='))
-b=float(input('b='))
-N_0=float(input('N0='))
-N.append(N_0)
-t.append(0)
-time=float(input('total time='))
-dt=float(input('time step='))
-
-f=open('problem1.6.txt','w')
-#calculation and storage
-for i in range(int(time/dt)):
-    N.append(N[i]+dt*(a*N[i]-b*(N[i])**2))#Attn:It is'**',not'^'
-    t.append(t[i]+dt)
-    print t[-1],N[-1]
-    print >> f,t[-1],N[-1]
-f.close()
+#exact solution
+def exact_N(a,b):
+    N=[]
+    t=[]
+    N_0=100#float(input('N0='))
+    N.append(N_0)
+    t.append(0)
+    time=2#float(input('total time='))
+    dt=0.001#float(input('time step='))
+    for i in range(int(time/dt)):
+        N.append(a*N_0*e**(a*t[i])/(a+b*N_0*(e**(a*t[i])-1)))#Attn:It is'**',not'^'
+        t.append(t[i]+dt)
+    return N
+N_exact0=exact_N(1,0)
+N_exact1=exact_N(1,0.01)
+N_exact2=exact_N(1,0.02)
 
 #plot 
-plot(t,N,'--',color='blue')
-legend(('N(numerical)'))
-title('Problem 1.6',fontsize=20)
+plot(t,N_0,'--',color='red')
+plot(t,N_exact0,color='red')
+plot(t,N_1,'--',color='green')
+plot(t,N_exact1,color='green')
+plot(t,N_2,'--',color='blue')
+plot(t,N_exact2,color='blue')
+legend(('b=0 numerical','b=0 exact','b=0.01 numerical','b=0.01 exact','b=0.02 numerical','b=0.02 exact'))
+title('Problem 1.6',fontsize=15)
 xlabel('t(year)')
 ylabel('N')
-
 savefig('problem1.6.png')
 show()
