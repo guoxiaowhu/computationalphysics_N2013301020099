51d068195d4f1f9bda4b5136ad68ea2835b3b5bd
guoxiaowhu <guoxiao1995@qq.com>
2016-05-24 13:59:09 +0800

    Add files via upload

--- problem5.7.py
+++ problem5.7.py
@@ -99,10 +99,9 @@ def SOR(L):
     for i in [a]:
         for j in range(a,b+1):
             V0[j][i]=1.0# j,i
-
     for i in [b]:
         for j in range(a,b+1):
-            V0[j][i]=-1.0
+            V0[j][i]=-1.0 
     VV=[]
     VV.append(V0)
     alpha=2.0/(1+pi/L)
@@ -112,36 +111,57 @@ def SOR(L):
     while 1:
         VV.append(V0)
         for i in range(1,L-1):
-            for j in range(1,L-1):
+            for j in range(1,L-1):               
                 VV[s+1][i][j]=(VV[s][i+1][j]+VV[s+1][i-1][j]+VV[s][i][j+1]+VV[s+1][i][j-1])/4.0
+                VV[s+1][i][j]=alpha*(VV[s+1][i][j]-VV[s][i][j])+VV[s][i][j]        
         for i in [a]:
             for j in range(a,b+1):
                 VV[s+1][j][i]=1.0
         for i in [b]:
             for j in range(a,b+1):
-                VV[s+1][j][i]=-1.0
+                VV[s+1][j][i]=-1.0         
         VV[s]=np.array(VV[s])
         VV[s+1]=np.array(VV[s+1])
         dVV=VV[s+1]-VV[s]
-        VV[s+1]=alpha*dVV+VV[s]
+
         dV=0
         for i in range(1,L-1):
             for j in range(1,L-1):
-                dV=dV+abs(alpha*dVV[i][j])
+
+                 dV=dV+abs(dVV[i][j])
         print dV,L 
-          
         s=s+1
         if dV<error*(L-1)**2 and s>1:
-        #if dV<error and s>1:
             break
     return s
+
+L=[]
+NJ=[]
+NGS=[]
+NSOR=[]
 f=open('problem5.7.txt','w')
-print >> f,'L','J','GS'
-for i in range(6,51,5):
-    print >> f,i,Jacobi(i),GS(i)
-print >> f,'L','SOR'
-for i in range(6,30,2):#without 31
-    print >> f,i,SOR(i)
+print >> f,'L','J','GS','SOR'
+for i in range(6,61,5):
+    J=Jacobi(i)
+    G=GS(i)
+    S=SOR(i)
+    L.append(i)
+    NJ.append(J)
+    NGS.append(G)
+    NSOR.append(S)
+    print >> f,i,J,G,S
 f.close()
-print 'end'
+plot(L,NJ)
+plot(L,NGS)
+plot(L,NSOR)
+scatter(L,NJ)
+scatter(L,NGS)
+scatter(L,NSOR)
+legend(('Jacobi method','GS method','SOR  method'),'upper left')
+title('3 different methods',fontsize=15)
+xlabel('L')
+ylim(0,1000)
+ylabel('N')
+savefig('different methods.png')
+show()
 
