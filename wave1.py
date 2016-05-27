5f140990cf184c270ab9c6e745a4861ca7d20e81
guoxiaowhu <guoxiao1995@qq.com>
2016-05-27 11:07:42 +0800

    Add files via upload

--- wave1.py
+++ wave1.py
@@ -8,13 +8,13 @@ dx=0.01
 c=300.0 #speed
 dt=dx/c
 length=int(1.0/dx)
-time=200
+time=3000
 k=1000
 y=[[0 for i in range(length)]for n in range(time)]#i represents x, n represents t
 #initialize
 for i in range(length):
-    y[0][i]=exp(-k*(i*dx-0.3)**2)
-    y[1][i]=exp(-k*(i*dx-0.3)**2)
+    y[0][i]=exp(-k*(i*dx-0.5-0.05)**2)
+    y[1][i]=exp(-k*(i*dx-0.5-0.05)**2)
 #for i in range(2*length/5):
 #    y[0][i]=3*i*dx
 #    y[1][i]=3*i*dx
@@ -42,38 +42,32 @@ ylabel('y and t')
 title('Waves on a string (fixed ends)')
 #text(0,2,'reflection and inversion')#'$F_D$'
 #savefig('wave 6.4.png')
-savefig('wave 6.2.png')
+#savefig('wave 6.2.png')
 #savefig('wave .png')
-figure()
+figure(figsize=[16,8])
+subplot(121)
 y5=[]
 t=array(range(time))*dt
 for n in range(time):
-    y5.append(y[n][5])
+    y5.append(y[n][4])
 plot(t,y5)
 xlabel('t/s')
 ylabel('y/m')
 title('vibration of a point on a string')
 text(0,0.56,r'a point at 5% of this string')
+subplot(122)
+p=abs(np.fft.rfft(y5))**2
+f = np.linspace(0, int(1/dt/2), len(p))
+plot(f, p)
+xlim(0,3000)
+xlabel('Frequency(Hz)')
+ylabel('Power')
+title('Power spectrum')
+#text(150,12000,'excited at center')
+text(150,12000,'excited at 5% from its center')
+
 #savefig('vibration 6.6 pluck.png')
-#savefig('vibration 6.6.png')
+savefig('vibration 6.6 fft 5.png')
 #savefig('vibration .png')
 show()
-'''
-fig = figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, V,rstride=1, cstride=1,linewidth=0)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('V')
-title('electric potential')
-savefig('electric potential J 3D.png')
-figure(figsize=[8,8])
-Q=quiver(X,Y,Ex,Ey,scale=100)
-xlim(-1,1)
-xlabel('x')
-ylim(-1,1)
-ylabel('y')
-title('electric field')
-savefig('electric field J.png')
-show()
-'''
+
